9da374644d082a431cee6aeaa83cb3dd8ca5d351
rahmanrifatt <74848658+rahmanrifatt@users.noreply.github.com>
2024-07-01 23:24:17 -0600

    Add files via upload

---
+++ server_gui_with_table_implementation_with_log4j/src/com/eGen/Astm.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eGen;
+
+/**
+ *
+ * @author eGen
+ */
+public class Astm {
+
+    public static final char STX=0x02;
+    public static final char LF=0xA;
+    public static final char CR=0xD;
+    public static final char ETX=0x03;
+    public static final char pipe=0x7C;
+    public static final char ENQ=0x05;
+    public static final char EOT=0x04;
+    public static final char ACK=0x06;
+    
+}
+
